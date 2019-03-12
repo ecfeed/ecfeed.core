884299bf5d3ae8d8704b7802da8f3fa252a801f0
Marek Kuczabinski <m.kuczabinski@radytek.com>
2019-03-12 15:28:12 +0100

    Generator N parser added.

---
+++ src/com/ecfeed/core/parser/GeneratorsNParser.java
@@ -0,0 +1,26 @@
+package com.ecfeed.core.parser;
+
+public class GeneratorsNParser {
+
+    private int fN = 2;
+
+    public GeneratorsNParser(String nStr) throws Exception {
+
+        if (nStr == null) {
+            fN = 2;
+            return;
+        }
+
+        try {
+            fN = Integer.parseInt(nStr);
+        } catch (Exception e) {
+            throw new Exception("Can not set N. " + e.getMessage());
+        }
+    }
+
+    public int getN() {
+        
+        return fN;
+    }
+
+}
