01194a78e9fe797998003124a044e532af5ae721
unknown <izulin@gmail.com>
2019-03-29 16:20:34 +0100

    first approach towards parsing of statements

--- src/com/ecfeed/core/evaluator/Sat4jEvaluator.java
+++ src/com/ecfeed/core/evaluator/Sat4jEvaluator.java
@@ -1,7 +1,7 @@
 package com.ecfeed.core.evaluator;
 
 import com.ecfeed.core.generators.api.IConstraintEvaluator;
-import com.ecfeed.core.model.IConstraint;
+import com.ecfeed.core.model.*;
 import com.ecfeed.core.utils.EvaluationResult;
 import org.sat4j.core.VecInt;
 import org.sat4j.minisat.SolverFactory;
@@ -10,44 +10,179 @@ import org.sat4j.specs.IProblem;
 import org.sat4j.specs.ISolver;
 import org.sat4j.specs.TimeoutException;
 
-//public class Sat4jEvaluator<E> implements IConstraintEvaluator<E> {
-public class Sat4jEvaluator{
+import java.util.*;
 
-    public static void main(String[] args) {
-        final int MAXVAR = 10000;
-        final int NBCLAUSES = 5000;
+public class Sat4jEvaluator implements IConstraintEvaluator<ChoiceNode> {
 
-        ISolver solver = SolverFactory.newDefault();
+    private List<ArrayList<Integer>> clauses;
+    private int firstUnusedID = 1;
+    private HashMap<AbstractStatement, Integer> statementToID;
 
 
-        try {
-            //prepare the solver to accept MAXVAR variables. MANDATORY for MAXSAT solving
-            solver.newVar(MAXVAR);
-            solver.setExpectedNumberOfClauses(NBCLAUSES);
-            solver.addClause(new VecInt(new int[]{1, 3, 7}));
-            solver.addClause(new VecInt(new int[]{1, 3, -7}));
-            solver.addClause(new VecInt(new int[]{1, -3, 7}));
-            solver.addClause(new VecInt(new int[]{1, -3, -7}));
-            solver.addClause(new VecInt(new int[]{-1, 3, 7}));
-            solver.addClause(new VecInt(new int[]{-1, 3, -7}));
-            solver.addClause(new VecInt(new int[]{-1, -3, 7}));
-            solver.addClause(new VecInt(new int[]{-1, -3, -7}));
-        } catch (ContradictionException e) {
-            System.out.println("Unsatisfiable (trivial)!");
+
+    public Sat4jEvaluator(Collection<Constraint> initConstraints) {
+        statementToID = new HashMap<>();
+        if(initConstraints != null) {
+            for (Constraint constraint : initConstraints) {
+                ParseConstraint(constraint);
+            }
         }
 
+    }
 
-        try {
-// we are done. Working now on the IProblem interface
-        IProblem problem = solver;
+    private int newID()
+    {
+        int ret = firstUnusedID;
+        firstUnusedID++;
+        return ret;
+    }
 
-            if (problem.isSatisfiable()) {
-                System.out.println("satisfiable");
-            } else {
-                System.out.println("not");
-            }
-        } catch (TimeoutException e) {
-            System.out.println("Timeout, sorry!");
+
+    private void ParseConstraint(Constraint constraint) {
+        if(constraint != null) {
+            AbstractStatement premise = constraint.getPremise(), consequence = constraint.getConsequence();
+            Integer premiseID=null, consequenceID=null;
+            try {
+                premiseID = (Integer)premise.accept(new ParseStatementVisitor());
+                consequenceID = (Integer)consequence.accept(new ParseStatementVisitor());
+            } catch (Exception e) {}
+            clauses.add(new ArrayList<>(Arrays.asList(-premiseID,consequenceID)));
         }
     }
+
+    class ParseStatementVisitor implements IStatementVisitor
+    {
+        @Override
+        public Object visit(StatementArray statement)
+        {
+            Integer myID = newID();
+            switch(statement.getOperator()){
+                case OR: // y = (x1 OR x2 OR .. OR xn) compiles to: (NOT x1 OR y) AND ... AND (NOT xn OR y) AND (x1 OR ... OR xn OR NOT y)
+                {
+                    ArrayList<Integer> bigClause = new ArrayList<>();
+                    for (AbstractStatement child : statement.getChildren()) {
+                        Integer childID = null;
+                        try {
+                            childID = (Integer) child.accept(new ParseStatementVisitor());
+                        } catch (Exception e) {
+                        }
+                        bigClause.add(childID);
+                        clauses.add(new ArrayList<>(Arrays.asList(-childID, myID))); //small clauses
+                    }
+                    bigClause.add(-myID);
+                    clauses.add(bigClause);
+                    break;
+                }
+                case AND: // y = (x1 AND x2 AND .. AND xn) compiles to: (x1 OR NOT y) AND ... AND (xn OR NOT y) AND (NOT x1 OR ... OR NOT xn OR y)
+                {
+                    ArrayList<Integer> bigClause = new ArrayList<>();
+                    for (AbstractStatement child : statement.getChildren()) {
+                        Integer childID = null;
+                        try {
+                            childID = (Integer) child.accept(new ParseStatementVisitor());
+                        } catch (Exception e) {
+                        }
+                        bigClause.add(-childID);
+                        clauses.add(new ArrayList<>(Arrays.asList(childID, -myID))); //small clauses
+                    }
+                    bigClause.add(myID);
+                    clauses.add(bigClause);
+                    break;
+                }
+            }
+//            statementToID.put(statement,myID); not really necessary, as we are never reusing the same statements
+            return myID;
+        }
+        @Override
+        public Object visit(RelationStatement statement)
+        {
+            return 0; //TODO
+        }
+        @Override
+        public Object visit(ExpectedValueStatement statement)
+        {
+            return 0; //TODO
+        }
+        @Override
+        public Object visit(StaticStatement statement)
+        {
+            Integer myID = newID();
+            if(statement.getValue() == EvaluationResult.TRUE)
+                clauses.add(new ArrayList<>(Collections.singletonList(myID)));
+            else
+                clauses.add(new ArrayList<>(Collections.singletonList(-myID)));
+            return myID;
+        }
+        @Override
+        public Object visit(LabelCondition statement)
+        {
+            return 0; //TODO
+        }
+        @Override
+        public Object visit(ChoiceCondition statement)
+        {
+            return 0; //TODO
+        }
+        @Override
+        public Object visit(ParameterCondition statement)
+        {
+            return 0; //TODO
+        }
+        @Override
+        public Object visit(ValueCondition statement)
+        {
+            return 0; //TODO
+        }
+    }
+
+    @Override
+    public EvaluationResult evaluate(List<ChoiceNode> valueAssignment)
+    {
+        return EvaluationResult.INSUFFICIENT_DATA; //TODO
+    }
+
+
+    @Override
+    public List<ChoiceNode> adapt(List<ChoiceNode> valueAssignment)
+    {
+        return new ArrayList<>(); //TODO
+    }
+
+//    public static void main(String[] args) {
+//        final int MAXVAR = 10000;
+//        final int NBCLAUSES = 5000;
+//
+//        ISolver solver = SolverFactory.newDefault();
+//
+//
+//        try {
+//            //prepare the solver to accept MAXVAR variables. MANDATORY for MAXSAT solving
+//            solver.newVar(MAXVAR);
+//            solver.setExpectedNumberOfClauses(NBCLAUSES);
+//            solver.addClause(new VecInt(new int[]{1, 3, 7}));
+//            solver.addClause(new VecInt(new int[]{1, 3, -7}));
+//            solver.addClause(new VecInt(new int[]{1, -3, 7}));
+//            solver.addClause(new VecInt(new int[]{1, -3, -7}));
+//            solver.addClause(new VecInt(new int[]{-1, 3, 7}));
+//            solver.addClause(new VecInt(new int[]{-1, 3, -7}));
+//            solver.addClause(new VecInt(new int[]{-1, -3, 7}));
+//            solver.addClause(new VecInt(new int[]{-1, -3, -7}));
+//        } catch (ContradictionException e) {
+//            System.out.println("Unsatisfiable (trivial)!");
+//        }
+//
+//
+//        try {
+//// we are done. Working now on the IProblem interface
+//        IProblem problem = solver;
+//
+//            if (problem.isSatisfiable()) {
+//                System.out.println("satisfiable");
+//            } else {
+//                System.out.println("not");
+//            }
+//        } catch (TimeoutException e) {
+//            System.out.println("Timeout, sorry!");
+//        }
+//    }
 }
--- src/com/ecfeed/core/generators/api/IConstraintEvaluator.java
+++ src/com/ecfeed/core/generators/api/IConstraintEvaluator.java
@@ -4,6 +4,6 @@ import java.util.List;
 import com.ecfeed.core.utils.EvaluationResult;
 
 public interface IConstraintEvaluator<E> {
-    public EvaluationResult evaluate(List<E> valueAssingment);
-    public List<E> adapt(List<E> valueAssingment);
+    public EvaluationResult evaluate(List<E> valueAssignment);
+    public List<E> adapt(List<E> valueAssignment);
 }
