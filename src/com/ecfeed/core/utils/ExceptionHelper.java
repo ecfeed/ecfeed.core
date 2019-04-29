b2efb7e24904eec759e065a9779b92bfc226efd2
Marek Kuczabinski <m.kuczabinski@radytek.com>
2019-04-29 13:53:28 +0200

    Creating error message.

--- src/com/ecfeed/core/utils/ExceptionHelper.java
+++ src/com/ecfeed/core/utils/ExceptionHelper.java
@@ -12,17 +12,21 @@ package com.ecfeed.core.utils;
 
 public class ExceptionHelper {
 
+	private static final int fMaxDepth = 5;
+	private static final String causedBy = " Caused by: ";
+	private static final String fNoException = "NO-EXCEPTION";
+
 	public static void reportRuntimeException(String message) {
+
 		SystemLogger.logThrow(message);
 		throw new RuntimeException(message);
 	}
 
 	public static void reportRuntimeException(String message, Exception e) {
 
-		RuntimeException runtimeException = new RuntimeException(message);
-		runtimeException.addSuppressed(e);
+		RuntimeException runtimeException = new RuntimeException(message, e);
 
-		SystemLogger.logThrow(message);
+		SystemLogger.logThrow(createErrorMessage(message, e));
 		throw runtimeException;
 	}
 
@@ -33,18 +37,70 @@ public class ExceptionHelper {
 	}
 
 	public static void reportRuntimeExceptionCanNotCreateObject() {
+
 		ExceptionHelper.reportRuntimeException("Can not create object.");
 	}
 
 	public static String createErrorMessage(String basicMessage, Exception e) {
 
-		String causedBy = " Caused by: ";
+		return basicMessage + "\n" + causedBy + createErrorMessage(e);
+	}
 
-		String message = basicMessage + causedBy + e.getMessage();
+	public static String createErrorMessage(Exception e) {
 
-		if (e.getCause() != null) {
-			message += (causedBy + e.getCause().getMessage());
+		return createErrorMessage(e, true);
+	}
+	
+	public static String createErrorMessage(Exception e, boolean addClassName) {
+		
+		return createErrorMessage(e, addClassName, true);
+	}
+
+	public static String createErrorMessage(Exception e, boolean addClassName, boolean oneLine) {
+		
+		if (e == null) {
+			return fNoException;
 		}
+		
+		String exceptionSeparator = "\n";
+		if (oneLine) {
+			exceptionSeparator = " ";
+		}
+
+		String message = getMessage(e, addClassName);
+
+		Throwable currentThrowable = (Throwable)e;
+		int depth = 0;
+
+		for (;;) {
+
+			Throwable nextThrowable = currentThrowable.getCause();
+
+			if (nextThrowable == null) {
+				return message;
+			}
+
+			message += (exceptionSeparator + causedBy + getMessage(nextThrowable, addClassName) );
+
+			currentThrowable = nextThrowable;
+
+			depth++;
+
+			if (depth >= fMaxDepth) {
+				return message;
+			}
+		}
+	}
+
+	private static String getMessage(Throwable e, boolean addClassName) {
+
+		String message = "";
+
+		if (addClassName) {
+			message += "[" + e.getClass().getName() + "] "; 
+		}
+
+		message += e.getMessage();
 
 		return message;
 	}
