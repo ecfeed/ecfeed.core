28b9283e76ad60838e834b32e79b22d6b23215e6
krzysztof skorupski <k.skorupski@testify.no>
2020-07-09 09:56:46 +0200

    GoUpDownAction

--- src/com/ecfeed/core/model/MethodNode.java
+++ src/com/ecfeed/core/model/MethodNode.java
@@ -151,11 +151,11 @@ public class MethodNode extends ParametersParentNode {
 			copy.addParameter(parameter.makeClone());
 		}
 
-		for(TestSuiteNode testSuite : fTestSuites){
-			TestSuiteNode tcase = testSuite.getCopy(copy);
-			if(tcase != null)
-				copy.addTestSuite(tcase);
-		}
+//		for(TestSuiteNode testSuite : fTestSuites){
+//			TestSuiteNode tcase = testSuite.getCopy(copy);
+//			if(tcase != null)
+//				copy.addTestSuite(tcase);
+//		}
 		
 		for(TestCaseNode testcase : fTestCases){
 			TestCaseNode tcase = testcase.getCopy(copy);
