2723fda7663cf781e36363577eea4d226401b8c5
Marek Kuczabinski <m.kuczabinski@radytek.com>
2020-08-13 13:04:17 +0200

    Parameter label added.

---
+++ src/com/ecfeed/core/model/AbstractParameterNodeHelper.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2016 ecFeed AS.                                                
+ * All rights reserved. This program and the accompanying materials              
+ * are made available under the terms of the Eclipse Public License v1.0         
+ * which accompanies this distribution, and is available at                      
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ *******************************************************************************/
+
+package com.ecfeed.core.model;
+
+import com.ecfeed.core.utils.SimpleTypeHelper;
+import com.ecfeed.core.utils.ViewMode;
+
+public abstract class AbstractParameterNodeHelper {
+
+	public static String createLabel(AbstractParameterNode abstractParameterNode, ViewMode viewMode) {
+		
+		String name = abstractParameterNode.getFullName();
+		String type = abstractParameterNode.getType();
+		
+		if (viewMode == ViewMode.SIMPLE) {
+			name = SimpleTypeHelper.convertTextFromJavaToSimpleConvention(name);
+			type = SimpleTypeHelper.convertJavaTypeToSimpleType(type);
+		}
+		
+		String label = name + ": " + type;
+		return label;
+	}
+}
