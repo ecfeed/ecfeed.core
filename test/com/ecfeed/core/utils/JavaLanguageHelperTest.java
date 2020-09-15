294886b49e3615c9d0f8e8aec0ccd8f832d80d65
Marek Kuczabinski <m.kuczabinski@radytek.com>
2020-09-15 14:02:05 +0200

    Validating type - test.

---
+++ test/com/ecfeed/core/utils/JavaLanguageHelperTest.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2016 ecFeed AS.                                                
+ * All rights reserved. This program and the accompanying materials              
+ * are made available under the terms of the Eclipse Public License v1.0         
+ * which accompanies this distribution, and is available at                      
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ *******************************************************************************/
+
+package com.ecfeed.core.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JavaLanguageHelperTest {
+
+	@Test
+	public void verifySeparatorsTest() {
+
+		String errorMessage = SimpleLanguageHelper.verifySeparators("abc");
+		assertNull(errorMessage);
+
+		errorMessage = SimpleLanguageHelper.verifySeparators("a_b");
+		assertNotNull(errorMessage);
+
+		errorMessage = SimpleLanguageHelper.verifySeparators(" ab");
+		assertNotNull(errorMessage);
+	}
+
+
+}
