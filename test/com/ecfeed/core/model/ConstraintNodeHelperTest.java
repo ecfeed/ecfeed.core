ebba6dd539220a627b2e34e91a1c27e543a4e94b
Marek Kuczabinski <m.kuczabinski@radytek.com>
2020-09-28 13:37:20 +0200

    Test added.

---
+++ test/com/ecfeed/core/model/ConstraintNodeHelperTest.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2016 ecFeed AS.                                                
+ * All rights reserved. This program and the accompanying materials              
+ * are made available under the terms of the Eclipse Public License v1.0         
+ * which accompanies this distribution, and is available at                      
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ *******************************************************************************/
+
+package com.ecfeed.core.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ConstraintNodeHelperTest {
+
+
+	// TODO SIMPLE-VIEW
+	
+	@Test
+	public void compare(){
+//		ConstraintNode c1 = new ConstraintNode("c", null, new Constraint("c", null, new StaticStatement(true, null), new StaticStatement(true, null)));
+//		ConstraintNode c2 = new ConstraintNode("c", null, new Constraint("c", null, new StaticStatement(true, null), new StaticStatement(true, null)));
+//
+//		assertTrue(c1.isMatch(c2));
+//
+//		c1.setName("c1");
+//		assertFalse(c1.isMatch(c2));
+//		c2.setName("c1");
+//		assertTrue(c1.isMatch(c2));
+//
+//		c1.getConstraint().setPremise(new StaticStatement(false, null));
+//		assertFalse(c1.isMatch(c2));
+//		c2.getConstraint().setPremise(new StaticStatement(false, null));
+//		assertTrue(c1.isMatch(c2));
+//
+//		c1.getConstraint().setConsequence(new StaticStatement(false, null));
+//		assertFalse(c1.isMatch(c2));
+//		c2.getConstraint().setConsequence(new StaticStatement(false, null));
+//		assertTrue(c1.isMatch(c2));
+	}
+}
