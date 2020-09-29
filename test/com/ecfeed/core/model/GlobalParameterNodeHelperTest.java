7e1deb5d8c27d5286f738a6449756bda52eee4c6
Marek Kuczabinski <m.kuczabinski@radytek.com>
2020-09-29 14:49:40 +0200

    Test file added.

---
+++ test/com/ecfeed/core/model/GlobalParameterNodeHelperTest.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2016 ecFeed AS.                                                
+ * All rights reserved. This program and the accompanying materials              
+ * are made available under the terms of the Eclipse Public License v1.0         
+ * which accompanies this distribution, and is available at                      
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ *******************************************************************************/
+
+package com.ecfeed.core.model;
+
+import com.ecfeed.core.utils.ExtLanguage;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GlobalParameterNodeHelperTest {
+
+	@Test
+	public void createSignatureTest(){
+
+//		GlobalParameterNode globalParameterNode = new GlobalParameterNode("global_1", null, "String");
+//
+//		String signature = GlobalParameterNodeHelper.createSignature(globalParameterNode, ExtLanguage.JAVA);
+//		assertEquals("String global_1", signature);
+//
+//		signature = GlobalParameterNodeHelper.createSignature(globalParameterNode, ExtLanguage.JAVA);
+//		assertEquals("Text global 1", signature);
+	}
+
+}
