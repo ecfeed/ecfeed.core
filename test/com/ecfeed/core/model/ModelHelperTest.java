570576f851f7e82bfca79100bd6c21e84662ef63
Marek Kuczabinski <m.kuczabinski@radytek.com>
2020-10-01 07:51:54 +0200

    Empty test added.

---
+++ test/com/ecfeed/core/model/ModelHelperTest.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2016 ecFeed AS.                                                
+ * All rights reserved. This program and the accompanying materials              
+ * are made available under the terms of the Eclipse Public License v1.0         
+ * which accompanies this distribution, and is available at                      
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ *******************************************************************************/
+
+package com.ecfeed.core.model;
+
+import com.ecfeed.core.testutils.RandomModelGenerator;
+import com.ecfeed.core.utils.EMathRelation;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ModelHelperTest {
+
+	@Test
+	public void newMethodNodeTest(){
+
+	}
+
+
+}
