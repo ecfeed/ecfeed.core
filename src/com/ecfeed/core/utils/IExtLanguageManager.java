89cac4259bf5c715a4d8f659cdda9afa5a00419e
Marek Kuczabinski <m.kuczabinski@radytek.com>
2020-10-06 11:04:03 +0200

    Comment.

--- src/com/ecfeed/core/utils/IExtLanguageManager.java
+++ src/com/ecfeed/core/utils/IExtLanguageManager.java
@@ -7,7 +7,7 @@ import com.ecfeed.core.model.ClassNode;
 
 public interface IExtLanguageManager {
 	
-	public String verifySeparatorsInName(String nameInExternalLanguage);
+	public String verifySeparatorsInName(String nameInExternalLanguage); // TODO SIMPLE-VIEW rename to verifySeparators
 	public boolean isLogicalTypeName(String type);
 	public String convertTextFromExtToIntrLanguage(String text);
 	public String convertTextFromIntrToExtLanguage(String text);
