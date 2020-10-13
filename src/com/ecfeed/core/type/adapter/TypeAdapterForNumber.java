4f73ebaa0b7d99abd839228f728ab302e81dcdec
Marek Kuczabinski <m.kuczabinski@radytek.com>
2020-10-13 11:25:13 +0200

    Constants used.

--- src/com/ecfeed/core/type/adapter/TypeAdapterForNumber.java
+++ src/com/ecfeed/core/type/adapter/TypeAdapterForNumber.java
@@ -27,44 +27,44 @@ public class TypeAdapterForNumber extends TypeAdapterForNumericType<Number>{
 
 	@Override
 	public String convertSingleValue(String value, ERunMode conversionMode) {
-		
+
 		String result = super.convertSpecialValue(value);
 
 		if (result != null) {
 			return result;
 		}
-		
-		if (StringHelper.isEqual("false", value)) { // TODO SIMPLE-VIEW - use constant
-			return "0";
+
+		if (StringHelper.isEqual(JavaLanguageHelper.SPECIAL_VALUE_FALSE, value)) {
+			return JavaLanguageHelper.SPECIAL_VALUE_ZERO;
 		}
 
-		if (StringHelper.isEqual("true", value)) { // TODO SIMPLE-VIEW - use constant
-			return "1";
+		if (StringHelper.isEqual(JavaLanguageHelper.SPECIAL_VALUE_TRUE, value)) {
+			return JavaLanguageHelper.SPECIAL_VALUE_ONE;
 		}
-		
+
 		try {
 			return String.valueOf(Long.parseLong(value));
 		} catch(NumberFormatException e){}
-		
+
 		try {
 			return String.valueOf(Double.parseDouble(value));
 		} catch(NumberFormatException e){}
-		
-		return "0";
+
+		return JavaLanguageHelper.SPECIAL_VALUE_ZERO;
 	}
 
 	@Override
 	public Number generateValue(String rangeTxt) {
 		String[] range = RangeHelper.splitToRange(rangeTxt);
 		String type = parseValue(range);
-		
+
 		if (StringHelper.isEqual(range[0], range[1])) {
 			return JavaLanguageHelper.parseValueToNumber(range[0], type, ERunMode.QUIET);
 		}
 
 		return generateNumber(range, type);
 	}
-	
+
 	private String parseValue(String[] range) {
 		try {
 			Integer.parseInt(range[0]);
@@ -72,10 +72,10 @@ public class TypeAdapterForNumber extends TypeAdapterForNumericType<Number>{
 			return JavaLanguageHelper.TYPE_NAME_INT;
 		} catch (NumberFormatException e1) {
 		}
-		
+
 		return parseLong(range);
 	}
-	
+
 	private String parseLong(String[] range) {
 		try {
 			Long.parseLong(range[0]);
@@ -83,10 +83,10 @@ public class TypeAdapterForNumber extends TypeAdapterForNumericType<Number>{
 			return JavaLanguageHelper.TYPE_NAME_LONG;
 		} catch (NumberFormatException e2) {
 		}
-		
+
 		return parseDouble(range);
 	}
-	
+
 	private String parseDouble(String[] range) {
 		try {
 			Double.parseDouble(range[0]);
@@ -94,37 +94,37 @@ public class TypeAdapterForNumber extends TypeAdapterForNumericType<Number>{
 			return JavaLanguageHelper.TYPE_NAME_DOUBLE;
 		} catch (NumberFormatException e3) {
 		}
-		
+
 		return parseDefault(range);
 	}
-	
+
 	private String parseDefault(String[] range) {
 		range[0] = "0";
 		range[1] = "0";
 		return JavaLanguageHelper.TYPE_NAME_INT;
 	}
-	
+
 	private Number generateNumber(String[] range, String type) {
 		switch(type) {
-			case JavaLanguageHelper.TYPE_NAME_INT :	return generateInt(range, type);
-			case JavaLanguageHelper.TYPE_NAME_LONG : return generateLong(range, type);
-			case JavaLanguageHelper.TYPE_NAME_DOUBLE : return generateDouble(range, type);
-			default : return 0;
+		case JavaLanguageHelper.TYPE_NAME_INT :	return generateInt(range, type);
+		case JavaLanguageHelper.TYPE_NAME_LONG : return generateLong(range, type);
+		case JavaLanguageHelper.TYPE_NAME_DOUBLE : return generateDouble(range, type);
+		default : return 0;
 		}
 	}
-	
+
 	private Integer generateInt(String[] range, String type) {
 		return ThreadLocalRandom.current().nextInt(
 				(Integer) JavaLanguageHelper.parseValueToNumber(range[0], type, ERunMode.QUIET),
 				(Integer) JavaLanguageHelper.parseValueToNumber(range[1], type, ERunMode.QUIET));
 	}
-	
+
 	private Long generateLong(String[] range, String type) {
 		return ThreadLocalRandom.current().nextLong(
 				(Long) JavaLanguageHelper.parseValueToNumber(range[0], type, ERunMode.QUIET),
 				(Long) JavaLanguageHelper.parseValueToNumber(range[1], type, ERunMode.QUIET));
 	}
-	
+
 	private Double generateDouble(String[] range, String type) {
 		return ThreadLocalRandom.current().nextDouble(
 				(Double) JavaLanguageHelper.parseValueToNumber(range[0], type, ERunMode.QUIET),
