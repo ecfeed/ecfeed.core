5d05d13b4d685d1248651229d613ca9ef8da4b7b
Marek Kuczabinski <m.kuczabinski@radytek.com>
2020-10-19 09:34:34 +0200

    Creating new parameters by operation. Test.

--- test/operations/MethodOperationAddParameterTest.java
+++ test/operations/MethodOperationAddParameterTest.java
@@ -29,7 +29,7 @@ public class MethodOperationAddParameterTest {
 
 		IExtLanguageManager  extLanguageManagerForSimple = new ExtLanguageManagerForSimple();
 
-		ClassNode classNode = new ClassNode("class1", null);
+		ClassNode classNode = new ClassNode("com.class1", null);
 
 		// add method with int parameter
 
@@ -69,10 +69,84 @@ public class MethodOperationAddParameterTest {
 			methodOperationAddParameter2.execute();
 			fail();
 		} catch (Exception e) {
+			TestHelper.checkExceptionMessage(
+					e,
+					"class1",
+					ClassNodeHelper.CONTAINS_METHOD_WITH_IDENTICAL_SIGNATURE);
+		}
+	}
 
+	@Test
+	public void addParameterForJava() {
+
+		IExtLanguageManager  extLanguageManagerForJava = new ExtLanguageManagerForJava();
+
+		ClassNode classNode = new ClassNode("com.class1", null);
+
+		// add method with int parameter
+
+		MethodNode methodNode1 = new MethodNode("method", null);
+		classNode.addMethod(methodNode1);
+
+		MethodParameterNode methodParameterNode1 =
+				new MethodParameterNode("arg0", "int", "0", false, null);
+
+		MethodOperationAddParameter methodOperationAddParameter1 =
+				new MethodOperationAddParameter(
+						methodNode1,
+						methodParameterNode1,
+						extLanguageManagerForJava);
+
+		try {
+			methodOperationAddParameter1.execute();
+		} catch (Exception e) {
+			fail();
 		}
 
+		// again add method with long parameter
 
+		MethodNode methodNode2 = new MethodNode("method", null);
+		classNode.addMethod(methodNode2);
+
+		MethodParameterNode methodParameterNode2 =
+				new MethodParameterNode("arg0", "long", "0", false, null);
+
+		MethodOperationAddParameter methodOperationAddParameter2 =
+				new MethodOperationAddParameter(
+						methodNode2,
+						methodParameterNode2,
+						extLanguageManagerForJava);
+
+		try {
+			methodOperationAddParameter2.execute();
+		} catch (Exception e) {
+			fail();
+		}
+
+		// again add the second method with long parameter
+
+		MethodNode methodNode3 = new MethodNode("method", null);
+		classNode.addMethod(methodNode3);
+
+		MethodParameterNode methodParameterNode3 =
+				new MethodParameterNode("arg0", "long", "0", false, null);
+
+		MethodOperationAddParameter methodOperationAddParameter3 =
+				new MethodOperationAddParameter(
+						methodNode3,
+						methodParameterNode3,
+						extLanguageManagerForJava);
+
+		try {
+			methodOperationAddParameter3.execute();
+			fail();
+		} catch (Exception e) {
+			TestHelper.checkExceptionMessage(
+					e,
+					"com.class1",
+					ClassNodeHelper.CONTAINS_METHOD_WITH_IDENTICAL_SIGNATURE);
+
+		}
 	}
 
 }
