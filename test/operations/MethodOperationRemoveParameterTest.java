5c0eaa0de7b7549022316f91305da7a09ddf96b8
Marek Kuczabinski <m.kuczabinski@radytek.com>
2020-10-19 10:45:43 +0200

    Remove parameter by operation. Test.

--- test/operations/MethodOperationRemoveParameterTest.java
+++ test/operations/MethodOperationRemoveParameterTest.java
@@ -50,4 +50,40 @@ public class MethodOperationRemoveParameterTest {
 		}
 	}
 
+	@Test
+	public void removeSecondOfTwoParametersInJavaMode() {
+
+		ClassNode classNode = new ClassNode("com.Class1", null);
+
+		MethodNode methodNode1 = new MethodNode("method", null);
+		classNode.addMethod(methodNode1);
+
+		MethodParameterNode methodParameterNode1 = new MethodParameterNode("arg0", "int", "0", false, null);
+		methodNode1.addParameter(methodParameterNode1);
+
+		MethodParameterNode methodParameterNode2 = new MethodParameterNode("arg1", "long", "0", false, null);
+		methodNode1.addParameter(methodParameterNode2);
+
+
+		MethodNode methodNode2= new MethodNode("method", null);
+		classNode.addMethod(methodNode2);
+
+		MethodParameterNode methodParameterNode3 = new MethodParameterNode("arg0", "int", "0", false, null);
+		methodNode2.addParameter(methodParameterNode3);
+
+
+		MethodOperationRemoveParameter methodOperationRemoveParameter
+				= new MethodOperationRemoveParameter(methodNode1, methodParameterNode2, new ExtLanguageManagerForJava());
+
+		try  {
+			methodOperationRemoveParameter.execute();
+			fail();
+		}  catch (Exception e) {
+			TestHelper.checkExceptionMessage(
+					e,
+					"Class1",
+					ClassNodeHelper.CONTAINS_METHOD_WITH_IDENTICAL_SIGNATURE);
+		}
+	}
+
 }
