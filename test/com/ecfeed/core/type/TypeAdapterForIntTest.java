a2e8d58cfb87b2fad586848d2de97715f9a52af6
Marek Kuczabinski <m.kuczabinski@radytek.com>
2020-10-28 11:16:41 +0100

    Type adapter for int. Test.

---
+++ test/com/ecfeed/core/type/TypeAdapterForIntTest.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2016 ecFeed AS.                                                
+ * All rights reserved. This program and the accompanying materials              
+ * are made available under the terms of the Eclipse Public License v1.0         
+ * which accompanies this distribution, and is available at                      
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ *******************************************************************************/
+
+package com.ecfeed.core.type;
+
+import com.ecfeed.core.type.adapter.TypeAdapterForInt;
+import com.ecfeed.core.type.adapter.TypeAdapterForNumericType;
+import com.ecfeed.core.type.adapter.TypeAdapterHelper;
+import com.ecfeed.core.utils.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class TypeAdapterForIntTest {
+
+	@Test
+	public void convertSingleValueForJavaTest() {
+
+		IExtLanguageManager extLanguageManagerForJava = new ExtLanguageManagerForJava();
+
+		TypeAdapterForInt typeAdapterForInt = new TypeAdapterForInt();
+
+		assertEquals("1", typeAdapterForInt.convertSingleValue("1", ERunMode.QUIET, extLanguageManagerForJava));
+		assertEquals("1", typeAdapterForInt.convertSingleValue("1.0", ERunMode.QUIET, extLanguageManagerForJava));
+		assertEquals("0", typeAdapterForInt.convertSingleValue("ab", ERunMode.QUIET, extLanguageManagerForJava));
+
+		try {
+			typeAdapterForInt.convertSingleValue("ab", ERunMode.WITH_EXCEPTION, extLanguageManagerForJava);
+			fail();
+		} catch (Exception e) {
+			TestHelper.checkExceptionMessage(e, TypeAdapterHelper.CANNOT_CONVERT_VALUE);
+		}
+
+		assertEquals("MAX_VALUE", typeAdapterForInt.convertSingleValue("MAX_VALUE", ERunMode.QUIET, extLanguageManagerForJava));
+	}
+
+	@Test
+	public void convertSingleValueForSimpleTest() {
+
+		IExtLanguageManager extLanguageManagerForSimple = new ExtLanguageManagerForSimple();
+
+		TypeAdapterForInt typeAdapterForInt = new TypeAdapterForInt();
+
+		assertEquals("1", typeAdapterForInt.convertSingleValue("1", ERunMode.QUIET, extLanguageManagerForSimple));
+		assertEquals("1", typeAdapterForInt.convertSingleValue("1.0", ERunMode.QUIET, extLanguageManagerForSimple));
+
+		// invalid value
+
+		assertEquals("0", typeAdapterForInt.convertSingleValue("ab", ERunMode.QUIET, extLanguageManagerForSimple));
+
+		try {
+			typeAdapterForInt.convertSingleValue("ab", ERunMode.WITH_EXCEPTION, extLanguageManagerForSimple);
+			fail();
+		} catch (Exception e) {
+			TestHelper.checkExceptionMessage(e, TypeAdapterHelper.CANNOT_CONVERT_VALUE);
+		}
+
+		// symbolic value
+
+		assertEquals("0", typeAdapterForInt.convertSingleValue("MAX_VALUE", ERunMode.QUIET, extLanguageManagerForSimple));
+
+		try {
+			typeAdapterForInt.convertSingleValue("MAX_VALUE", ERunMode.WITH_EXCEPTION, extLanguageManagerForSimple);
+			fail();
+		} catch (Exception e) {
+			TestHelper.checkExceptionMessage(e, TypeAdapterForNumericType.SPECIAL_VALUES_ARE_NOT_ALLOWED);
+		}
+	}
+
+}
