b0eac9c4a5a3e0adc44b4b3a419f8e1232291b92
Marek Kuczabinski <m.kuczabinski@radytek.com>
2020-12-11 12:14:03 +0100

    Test added - generating xml with standard constraint.

--- test/com/ecfeed/core/serialization/XmlUpgradeTemporaryTest.java
+++ test/com/ecfeed/core/serialization/XmlUpgradeTemporaryTest.java
@@ -37,7 +37,78 @@ import static org.junit.Assert.fail;
 public class XmlUpgradeTemporaryTest {
 
 	@Test
-	public void generateConstraintExampleInVersion3() {
+	public void generateFilteringConstraintExampleInVersion3() {
+
+		RootNode root = new RootNode("root", null, 3);
+
+		ClassNode classNode = new ClassNode("classNode", null);
+		root.addClass(classNode);
+
+		MethodNode methodNode = new MethodNode("method", null);
+		classNode.addMethod(methodNode);
+
+		// method parameter 1 node with choice
+
+		MethodParameterNode methodParameterNode1 = new MethodParameterNode(
+				"par1",
+				"int",
+				"1",
+				false,
+				null);
+
+		methodNode.addParameter(methodParameterNode1);
+
+		ChoiceNode choiceNode11 = new ChoiceNode("choice11", "11",  null);
+		methodParameterNode1.addChoice(choiceNode11);
+
+		// method parameter 2 node with choice
+
+		MethodParameterNode methodParameterNode2 = new MethodParameterNode(
+				"par2",
+				"int",
+				"2",
+				false,
+				null);
+
+		methodNode.addParameter(methodParameterNode2);
+
+		ChoiceNode choiceNode21 = new ChoiceNode("choice21", "21",  null);
+		methodParameterNode2.addChoice(choiceNode21);
+
+		// constraint
+
+		AbstractStatement precondition =
+				RelationStatement.createStatementWithChoiceCondition(methodParameterNode1, EMathRelation.EQUAL, choiceNode11);
+
+
+		AbstractStatement postcondition =
+				RelationStatement.createStatementWithChoiceCondition(methodParameterNode2, EMathRelation.EQUAL, choiceNode21);
+
+		Constraint constraint = new Constraint(
+				"constraint",
+				null,
+				precondition,
+				postcondition);
+
+		ConstraintNode constraintNode = new ConstraintNode("cn", constraint, null);
+		methodNode.addConstraint(constraintNode);
+
+		// serializing to stream
+
+		ByteArrayOutputStream ostream = new ByteArrayOutputStream();
+		ModelSerializer serializer = new ModelSerializer(ostream, 3);
+
+		try {
+			serializer.serialize(root);
+		} catch (Exception e) {
+			fail();
+		}
+
+		System.out.println(ostream.toString());
+	}
+
+	@Test
+	public void generateExpectedValueConstraintExampleInVersion3() {
 
 		RootNode root = new RootNode("root", null, 3);
 
@@ -92,4 +163,6 @@ public class XmlUpgradeTemporaryTest {
 
 		//System.out.println(ostream.toString());
 	}
+
+
 }
