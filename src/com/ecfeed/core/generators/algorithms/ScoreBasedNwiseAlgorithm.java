29644da1c6be4fe224e45f8812c8d9f0f7258869
Shuai Wang <shuai.wang@testify.no>
2021-02-24 08:49:42 +0100

    Update

--- src/com/ecfeed/core/generators/algorithms/ScoreBasedNwiseAlgorithm.java
+++ src/com/ecfeed/core/generators/algorithms/ScoreBasedNwiseAlgorithm.java
@@ -1,38 +1,14 @@
 package com.ecfeed.core.generators.algorithms;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import com.ecfeed.core.generators.api.GeneratorException;
 import com.ecfeed.core.generators.api.IConstraintEvaluator;
 
 public class ScoreBasedNwiseAlgorithm<E> extends AbstractScoreBasedAlgorithm<E> {
-
-	private int fArgCount;
-	private NwiseScoreEvaluator<E> fNwiseScoreEvaluator;
 	
-	public ScoreBasedNwiseAlgorithm(List<List<E>> input, NwiseScoreEvaluator<E> nwiseScoreEvaluator, int argCount, IConstraintEvaluator<E> constraintEvaluator) throws GeneratorException {
-		super(input, nwiseScoreEvaluator, argCount, constraintEvaluator);
-		this.fArgCount = argCount;
-		this.fNwiseScoreEvaluator = nwiseScoreEvaluator;
-	}
-
-	// Generate a test suite which can cover all the fArgCount-tuples
-	public List<List<E>> generateTestSuite () throws GeneratorException {
-		fHistorydimensionOrder.clear();
-	      List<List<E>> testSuite = new ArrayList<>();
-	      while (fNwiseScoreEvaluator.existNDimensions(fArgCount)){
-	            List<E> test = nextTest();
-	            if (test!= null){
-	            	testSuite.add(test);
-	            }
-	        }
-	      Set<List<E>> set = new HashSet<List<E>>(testSuite);
-	      testSuite = new ArrayList<List<E>>(set);
-	      
-	      return testSuite;
+	public ScoreBasedNwiseAlgorithm(List<List<E>> input, int argCount, IConstraintEvaluator<E> constraintEvaluator) throws GeneratorException {
+		super(input, new NwiseScoreEvaluator<E>(input, constraintEvaluator, argCount), argCount, constraintEvaluator);
 	}
 	
 }
