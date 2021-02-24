58f39806cdd69b920de0cc2c65efbae4c676c807
Shuai Wang <shuai.wang@testify.no>
2021-02-24 08:49:19 +0100

    Update

--- src/com/ecfeed/core/generators/algorithms/ScoreBasedAlgorithmMock.java
+++ src/com/ecfeed/core/generators/algorithms/ScoreBasedAlgorithmMock.java
@@ -1,41 +1,15 @@
 package com.ecfeed.core.generators.algorithms;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
 import com.ecfeed.core.generators.api.GeneratorException;
 import com.ecfeed.core.generators.api.IConstraintEvaluator;
 import com.ecfeed.core.utils.EvaluationResult;
 
 public class ScoreBasedAlgorithmMock<E> extends AbstractScoreBasedAlgorithm<E> {
-
-	private int fArgCount;
-	private NwiseScoreEvaluator<E> fNwiseScoreEvaluator;
-
-	public ScoreBasedAlgorithmMock(List<List<E>> input, NwiseScoreEvaluator<E> nwiseScoreEvaluator, int argCount,
-			IConstraintEvaluator<E> constraintEvaluator) throws GeneratorException {
-		super(input, nwiseScoreEvaluator, argCount, constraintEvaluator);
-		this.fArgCount = argCount;
-		this.fNwiseScoreEvaluator = nwiseScoreEvaluator;
-	}
-
-	// Generate a test suite which can cover all the fArgCount-tuples
-	public List<List<E>> generateTestSuite() throws GeneratorException {
-		fHistorydimensionOrder.clear();
-		List<List<E>> testSuite = new ArrayList<>();
-		while (fNwiseScoreEvaluator.existNDimensions(fArgCount)) {
-			List<E> test = nextTest();
-			if (test != null) {
-				testSuite.add(test);
-			}
+	
+	public ScoreBasedAlgorithmMock(List<List<E>> input, int argCount, IConstraintEvaluator<E> constraintEvaluator) throws GeneratorException {
+		super(input, new NwiseScoreEvaluatorMock<E>(input, constraintEvaluator, argCount), argCount, constraintEvaluator);
 		}
-		Set<List<E>> set = new HashSet<List<E>>(testSuite);
-		testSuite = new ArrayList<List<E>>(set);
-
-		return testSuite;
-	}
 
 	@Override
 	public EvaluationResult constraintCheck(List<E> tuple) {
@@ -44,5 +18,4 @@ public class ScoreBasedAlgorithmMock<E> extends AbstractScoreBasedAlgorithm<E> {
 		else
 			return EvaluationResult.FALSE;
 	}
-
 }
