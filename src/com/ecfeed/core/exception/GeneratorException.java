7fdf3b5c69d621da591fe8ea92f514a67a32432d
Marek Kuczabinski <m.kuczabinski@radytek.com>
2021-03-08 12:17:24 +0100

    Stream messge removed.

--- src/com/ecfeed/core/exception/GeneratorException.java
+++ src/com/ecfeed/core/exception/GeneratorException.java
@@ -6,15 +6,6 @@ public class GeneratorException extends RuntimeException {
 	 * 
 	 */
 	private static final long serialVersionUID = 7245190958322769521L;
-	private String streamMessage = ""; // TODO GEN-ERR-HANDLING - remove ?
-
-    public String getStreamMessage() {
-        return this.streamMessage;
-    }
-
-    public void setStreamMessage(String streamMessage) {
-        this.streamMessage = streamMessage;
-    }
 
     public GeneratorException(String errorMessage) {
         super(errorMessage);
