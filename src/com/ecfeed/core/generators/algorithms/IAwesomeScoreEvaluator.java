04c4a38c1b6d3279168fd188b4e3da6e517d057f
Marek Kuczabinski <m.kuczabinski@testify.no>
2021-05-19 10:18:36 +0200

    Draft interface for score evaluator added.

---
+++ src/com/ecfeed/core/generators/algorithms/IAwesomeScoreEvaluator.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2016 ecFeed AS.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+
+package com.ecfeed.core.generators.algorithms;
+
+public class IAwesomeScoreEvaluator<E> {
+
+//	public void initialize(List<List<E>> input, IConstraintEvaluator<E> constraintEvaluator) throws GeneratorException;
+//
+//	public int getInitialNTupleCount();
+//	public int getCurrentNTupleCount();
+//
+//	public int getScore(List<E> tuple);
+//	public void updateScores(List<E> test);
+
+}
