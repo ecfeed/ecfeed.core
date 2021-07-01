309982094c22a566da75a10a9b658d3359bc7a23
Marek Kuczabinski <m.kuczabinski@testify.no>
2021-07-01 11:39:23 +0200

    Model parser for class created.

---
+++ src/com/ecfeed/core/model/serialization/ModelParserForClass.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2016 ecFeed AS.                                                
+ * All rights reserved. This program and the accompanying materials              
+ * are made available under the terms of the Eclipse Public License v1.0         
+ * which accompanies this distribution, and is available at                      
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ *******************************************************************************/
+
+package com.ecfeed.core.model.serialization;
+
+import static com.ecfeed.core.model.serialization.SerializationConstants.CLASS_NODE_NAME;
+
+import java.util.Optional;
+
+import com.ecfeed.core.model.ClassNode;
+import com.ecfeed.core.model.GlobalParameterNode;
+import com.ecfeed.core.model.MethodNode;
+import com.ecfeed.core.model.RootNode;
+import com.ecfeed.core.utils.BooleanHolder;
+import com.ecfeed.core.utils.ListOfStrings;
+import com.ecfeed.core.utils.StringHolder;
+
+import nu.xom.Element;
+
+public class ModelParserForClass {
+
+	private WhiteCharConverter fWhiteCharConverter = new WhiteCharConverter();
+
+	public ModelParserForClass() {
+	}
+	
+	public Optional<ClassNode> parseClass(
+			Element classElement, RootNode parent, ListOfStrings errorList) throws ParserException {
+
+		String name;
+		
+		try {
+			ModelParserHelper.assertNodeTag(classElement.getQualifiedName(), CLASS_NODE_NAME, errorList);
+			name = ModelParserHelper.getElementName(classElement, fWhiteCharConverter, errorList);
+		} catch (ParserException e) {
+			return Optional.empty();
+		}
+
+		BooleanHolder runOnAndroidHolder = new BooleanHolder(false);
+		StringHolder androidBaseRunnerHolder = new StringHolder(); 
+
+		ClassNode targetClassNode = 
+				new ClassNode(
+						name, 
+						parent.getModelChangeRegistrator(), 
+						runOnAndroidHolder.get(), 
+						androidBaseRunnerHolder.get());
+
+		targetClassNode.setDescription(ModelParserHelper.parseComments(classElement, fWhiteCharConverter));
+		//we need to do it here, so the backward search for global parameters will work
+		targetClassNode.setParent(parent);
+
+		
+		ModelParserForGlobalParameter modelParserForGlobalParameter = new ModelParserForGlobalParameter();
+		//parameters must be parsed before classes
+		for (Element child : ModelParserHelper.getIterableChildren(classElement, SerializationHelperVersion1.getParameterNodeName())) {
+			Optional<GlobalParameterNode> node = modelParserForGlobalParameter.parseGlobalParameter(child, targetClassNode.getModelChangeRegistrator(), errorList);
+			if (node.isPresent()) {
+				targetClassNode.addParameter(node.get());
+			}
+		}
+
+		ModelParserForMethod modelParserForMethod = new ModelParserForMethod();
+		
+		for (Element child : ModelParserHelper.getIterableChildren(classElement, SerializationConstants.METHOD_NODE_NAME)) {
+			Optional<MethodNode> node = modelParserForMethod.parseMethod(child, targetClassNode, errorList);
+			if (node.isPresent()) {
+				targetClassNode.addMethod(node.get());
+			}
+		}
+
+		return Optional.ofNullable(targetClassNode);
+	}
+
+}
