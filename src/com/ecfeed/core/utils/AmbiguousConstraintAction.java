71ae2fd56cf02b72521091603a3d8718439b0a16
Marek Kuczabinski <m.kuczabinski@testify.no>
2022-01-14 16:51:02 +0100

    Parsing code and description of ambiguous constraint action.

--- src/com/ecfeed/core/utils/AmbiguousConstraintAction.java
+++ src/com/ecfeed/core/utils/AmbiguousConstraintAction.java
@@ -2,42 +2,56 @@ package com.ecfeed.core.utils;
 
 public enum AmbiguousConstraintAction {
 
-    EXCLUDE("EXCLUDE", "Exclude"),
-    EVALUATE("EVALUATE", "Evaluate"),
-    INCLUDE("INCLUDE", "Include");
+	EXCLUDE("EXCLUDE", "Exclude"),
+	EVALUATE("EVALUATE", "Evaluate"),
+	INCLUDE("INCLUDE", "Include");
 
-    private String fCode;
-    private String fDescription;
+	private String fCode;
+	private String fDescription;
 
-    AmbiguousConstraintAction(String code, String description) {
+	AmbiguousConstraintAction(String code, String description) {
 
-        fCode =  code;
-        fDescription = description;
-    }
+		fCode =  code;
+		fDescription = description;
+	}
 
-    public String getCode() {
-        return fCode;
-    }
+	public String getCode() {
+		return fCode;
+	}
 
-    public String getDescription() {
-        return fDescription;
-    }
+	public String getDescription() {
+		return fDescription;
+	}
 
-    public static String[] getDescriptions() {
-        return new String[] {
-                AmbiguousConstraintAction.EXCLUDE.getDescription(),
-                AmbiguousConstraintAction.EVALUATE.getDescription(),
-                AmbiguousConstraintAction.INCLUDE.getDescription()};
-    }
+	public static String[] getDescriptions() {
+		return new String[] {
+				AmbiguousConstraintAction.EXCLUDE.getDescription(),
+				AmbiguousConstraintAction.EVALUATE.getDescription(),
+				AmbiguousConstraintAction.INCLUDE.getDescription()};
+	}
 
-    public static AmbiguousConstraintAction parse(String description) {
+	public static AmbiguousConstraintAction parseDescription(String description) {
 
-        try {
-            AmbiguousConstraintAction result = AmbiguousConstraintAction.valueOf(description);
-            return result;
-        } catch (Exception e) {
-            return null;
-        }
-    }
+		for (AmbiguousConstraintAction action : AmbiguousConstraintAction.values()) {
+
+			if (action.getDescription().equals(description)) {
+				return action;
+			}
+		}
+
+		return null;
+	}
+
+	public static AmbiguousConstraintAction parseCode(String description) {
+
+		for (AmbiguousConstraintAction action : AmbiguousConstraintAction.values()) {
+
+			if (action.getCode().equals(description)) {
+				return action;
+			}
+		}
+
+		return null;
+	}
 
 }
