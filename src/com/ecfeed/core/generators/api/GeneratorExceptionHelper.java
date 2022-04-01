9f20b6c0cf8e5a625b88a488d71ffb7b794e0559
Marek Kuczabinski <m.kuczabinski@testify.no>
2022-04-01 08:01:42 +0200

    Setting type of exception thrown by generator.

--- src/com/ecfeed/core/generators/api/GeneratorExceptionHelper.java
+++ src/com/ecfeed/core/generators/api/GeneratorExceptionHelper.java
@@ -3,8 +3,30 @@ package com.ecfeed.core.generators.api;
 import com.ecfeed.core.utils.ExceptionHelper;
 
 public class GeneratorExceptionHelper {
+
+    public enum ThrownExceptionType {
+        CLIENT,
+        RUNTIME
+    }
+
+    // generator used in RAP should throw runtime exceptions in case of invalid request
+    // because our frontend controlls the data
+    // in case of remote generator it is the client/user who creates the request
+    // and then client exception should be thrown
+    // which is handled differently in generator service than runtime exception
+    // (we do not log client exceptions in standard logging mode)
+    private static ThrownExceptionType fThrownExceptionType = ThrownExceptionType.RUNTIME;
+
+    public static void setThrownExceptionType(ThrownExceptionType thrownExceptionType) {
+        fThrownExceptionType = thrownExceptionType;
+    }
+
     public static void reportException(String s) {
 
-        ExceptionHelper.reportClientException(s);
+        if (fThrownExceptionType == ThrownExceptionType.CLIENT) {
+            ExceptionHelper.reportClientException(s);
+        } else {
+            ExceptionHelper.reportRuntimeException(s);
+        }
     }
 }
