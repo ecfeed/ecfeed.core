48a575d9f02c84e79af09feef07556c40544e605
Marek Kuczabinski <m.kuczabinski@testify.no>
2022-05-25 10:46:26 +0200

    Copying choice trees. Test for many levels added.

--- test/com/ecfeed/core/model/ChoicesParentNodeHelperTest.java
+++ test/com/ecfeed/core/model/ChoicesParentNodeHelperTest.java
@@ -19,17 +19,17 @@ import org.junit.Test;
 public class ChoicesParentNodeHelperTest {
 
 	@Test
-	public void copyOneLevelOfChoices(){
+	public void copyChoicesOfOneLevel(){
 
 		// metod node 
-		
+
 		MethodNode methodNode = new MethodNode("M", null);
-		
+
 		// source parameter with two choices
-		
+
 		MethodParameterNode methodParameterNode1 = 
 				MethodNodeHelper.addParameterToMethod(methodNode, "P1", "String");
-		
+
 		ChoiceNode choiceNode1 = 
 				MethodParameterNodeHelper.addChoiceToMethodParameter(
 						methodParameterNode1, "C1", "1");
@@ -37,30 +37,91 @@ public class ChoicesParentNodeHelperTest {
 		ChoiceNode choiceNode2 = 
 				MethodParameterNodeHelper.addChoiceToMethodParameter(
 						methodParameterNode1, "C2", "1");
-		
+
+		ChoiceNode choiceNode3 = 
+				MethodParameterNodeHelper.addChoiceToMethodParameter(
+						methodParameterNode1, "C3", "1");
+
+
 		// destination parameter 
-		
+
 		MethodParameterNode methodParameterNode2 = 
 				MethodNodeHelper.addParameterToMethod(methodNode, "P2", "String");
-		
+
 
 		// creating copy
-		
+
 		ChoicesParentNodeHelper.createCopyOfChoicesTree(methodParameterNode1, methodParameterNode2);
-		
+
 		// checks
-		
-		assertEquals(2, methodParameterNode1.getChoices().size());
-		
+
+		assertEquals(3, methodParameterNode1.getChoices().size());
+
 		List<ChoiceNode> copiedChoices = methodParameterNode2.getChoices();
-		assertEquals(2, copiedChoices.size());
+		assertEquals(3, copiedChoices.size());
 
 		ChoiceNode resultChoiceNode1 = copiedChoices.get(0);
 		assertEquals(choiceNode1.getName(), resultChoiceNode1.getName());
 
 		ChoiceNode resultChoiceNode2 = copiedChoices.get(1);
 		assertEquals(choiceNode2.getName(), resultChoiceNode2.getName());
+
+		ChoiceNode resultChoiceNode3 = copiedChoices.get(2);
+		assertEquals(choiceNode3.getName(), resultChoiceNode3.getName());
 	}
 
+	@Test
+	public void copyChoicesOfManyLevels(){
+
+		// metod node 
+
+		MethodNode methodNode = new MethodNode("M", null);
+
+		// source parameter with two choices
+
+		MethodParameterNode methodParameterNode1 = 
+				MethodNodeHelper.addParameterToMethod(methodNode, "P1", "String");
+
+		ChoiceNode choiceNode1 = 
+				MethodParameterNodeHelper.addChoiceToMethodParameter(
+						methodParameterNode1, "C1", "1");
+
+		ChoiceNode choiceNode2 = 
+				ChoiceNodeHelper.addChoiceToChoice(choiceNode1, "C2", "1");
+
+		ChoiceNode choiceNode3 = 
+				ChoiceNodeHelper.addChoiceToChoice(choiceNode2, "C3", "1");
+
+		// destination parameter 
+
+		MethodParameterNode methodParameterNode2 = 
+				MethodNodeHelper.addParameterToMethod(methodNode, "P2", "String");
+
+		// creating copy
+
+		ChoicesParentNodeHelper.createCopyOfChoicesTree(methodParameterNode1, methodParameterNode2);
+
+		// check choice 1
+
+		assertEquals(1, methodParameterNode1.getChoices().size());
+
+		List<ChoiceNode> copiedChoices1 = methodParameterNode2.getChoices();
+		assertEquals(1, copiedChoices1.size());
+
+		ChoiceNode resultChoiceNode1 = copiedChoices1.get(0);
+		assertEquals(choiceNode1.getName(), resultChoiceNode1.getName());
+
+		// check choice 2
+
+		List<ChoiceNode> copiedChoices2 = resultChoiceNode1.getChoices();
+		ChoiceNode resultChoiceNode2 = copiedChoices2.get(0);
+		assertEquals(choiceNode2.getName(), resultChoiceNode2.getName());
+
+		// check choice 3
+
+		List<ChoiceNode> copiedChoices3 = resultChoiceNode2.getChoices();
+		ChoiceNode resultChoiceNode3 = copiedChoices3.get(0);
+		assertEquals(choiceNode3.getName(), resultChoiceNode3.getName());
+	}
 
 }
