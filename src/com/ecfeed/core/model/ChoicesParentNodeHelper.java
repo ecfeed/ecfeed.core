57e6673c32aa94b8011ddf06186efbfe55926e3c
Marek Kuczabinski <m.kuczabinski@testify.no>
2022-05-25 10:09:53 +0200

    Creating copy of choices tree.

--- src/com/ecfeed/core/model/ChoicesParentNodeHelper.java
+++ src/com/ecfeed/core/model/ChoicesParentNodeHelper.java
@@ -10,26 +10,46 @@
 
 package com.ecfeed.core.model;
 
+import java.util.List;
+
 public abstract class ChoicesParentNodeHelper {
 
+	public static void createCopyOfChoicesTree(ChoicesParentNode srcParentNode, ChoicesParentNode dstParentNode) {
+
+		List<ChoiceNode> childChoiceNodes = srcParentNode.getChoices();
+
+		if (childChoiceNodes.size() == 0) {
+			return;
+		}
+
+		for (ChoiceNode choiceNode : childChoiceNodes) {
+
+			ChoiceNode clonedChoiceNode = choiceNode.makeClone();
+
+			dstParentNode.addChoice(clonedChoiceNode);
+
+			createCopyOfChoicesTree(choiceNode, clonedChoiceNode);
+		}
+	}
+
 	public static boolean isMatch(ChoicesParentNode choicesParentNode1, ChoicesParentNode choicesParentNode2) {
-		
+
 		if (choicesParentNode1 == null && choicesParentNode2 == null) {
 			return true;
 		}
-		
+
 		if (choicesParentNode1 == null && choicesParentNode2 != null) {
 			return false;
 		}
-		
+
 		if (choicesParentNode1 != null && choicesParentNode2 == null) {
 			return false;
 		}
-		
+
 		if (choicesParentNode1.isMatch(choicesParentNode2)) {
 			return true;
 		}
-		
+
 		return false;
 	}
 
