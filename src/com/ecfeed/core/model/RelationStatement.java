f43562346c4155ba351fe030c6f2a6dedf7be5e1
Krzysztof <k.skorupski@testify.no>
2022-07-11 09:25:35 +0200

    logger merge fix

--- src/com/ecfeed/core/model/RelationStatement.java
+++ src/com/ecfeed/core/model/RelationStatement.java
@@ -19,12 +19,12 @@ import com.ecfeed.core.utils.ExceptionHelper;
 import com.ecfeed.core.utils.IExtLanguageManager;
 import com.ecfeed.core.utils.IParameterConversionItemPart;
 import com.ecfeed.core.utils.JavaLanguageHelper;
+import com.ecfeed.core.utils.LogHelperCore;
 import com.ecfeed.core.utils.MessageStack;
 import com.ecfeed.core.utils.ParameterConversionItem;
 import com.ecfeed.core.utils.ParameterConversionItemPartForChoice;
 import com.ecfeed.core.utils.ParameterConversionItemPartForLabel;
 import com.ecfeed.core.utils.StringHelper;
-import com.ecfeed.core.utils.SystemLogger;
 
 public class RelationStatement extends AbstractStatement implements IRelationalStatement{
 
