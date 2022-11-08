6e351ff427ed226a7412859ca60beba9af810cae
Marek Kuczabinski <m.kuczabinski@testify.no>
2022-11-08 11:26:44 +0100

    Class ChoicesParentNode removed.

--- src/com/ecfeed/core/model/ChoicesParentNode.java
+++ src/com/ecfeed/core/model/ChoicesParentNode.java
@@ -1,196 +1,196 @@
-/*******************************************************************************
- *
- * Copyright (c) 2016 ecFeed AS.                                                
- * All rights reserved. This program and the accompanying materials              
- * are made available under the terms of the Eclipse Public License v1.0         
- * which accompanies this distribution, and is available at                      
- * http://www.eclipse.org/legal/epl-v10.html 
- *  
- *******************************************************************************/
-
-package com.ecfeed.core.model;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-public abstract class ChoicesParentNode extends AbstractNode implements IChoicesParentNode { // TODO MO-RE remove class
-
-	ChoicesListHolder fChoicesListHolder;
-	
-	public ChoicesParentNode(String name, IModelChangeRegistrator modelChangeRegistrator) {
-		super(name, modelChangeRegistrator);
-
-		fChoicesListHolder = new ChoicesListHolder(modelChangeRegistrator);
-	}
-
-	@Override
-	public List<IAbstractNode> getChildren() {
-
-		return new ArrayList<IAbstractNode>(getChoices());
-	}
-
-	@Override
-	public int getChildrenCount() {
-
-		return getChoiceCount();
-	}
-
-	@Override
-	public boolean isMatch(IAbstractNode other) {
-
-		if (other instanceof ChoicesParentNode == false) {
-			return false;
-		}
-
-		ChoicesParentNode otherChoicesParentNode = (ChoicesParentNode)other;
-
-		if (!fChoicesListHolder.isMatch(otherChoicesParentNode.fChoicesListHolder)) {
-			return false;
-		}
-
-		boolean isMatch = super.isMatch(other);
-
-		if (!isMatch) {
-			return false;
-		}
-		return true;
-	}
-
-	@Override
-	public void addChoice(ChoiceNode choiceToAdd) {
-
-		fChoicesListHolder.addChoice(choiceToAdd, this);
-	}
-
-	@Override
-	public void addChoice(ChoiceNode choiceToAdd, int index) {
-
-		fChoicesListHolder.addChoice(choiceToAdd, index, this);
-		registerChange();
-	}
-
-	@Override
-	public void addChoices(List<ChoiceNode> choicesToAdd) {
-
-		fChoicesListHolder.addChoices(choicesToAdd, this);
-		registerChange();
-	}
-
-	@Override
-	public List<ChoiceNode> getChoices() {
-
-		return fChoicesListHolder.getChoices();
-	}
-
-	@Override
-	public int getChoiceCount() {
-
-		return getChoices().size();
-	}
-
-	public List<ChoiceNode> getChoicesWithCopies() { // TODO MO-RE do we need this ?
-
-		return getChoices();
-	}
-
-	@Override
-	public ChoiceNode getChoice(String qualifiedName) {
-
-		return (ChoiceNode)getChild(qualifiedName);
-	}
-
-	@Override
-	public int getChoiceIndex(String choiceNameToFind) {
-
-		return fChoicesListHolder.getChoiceIndex(choiceNameToFind);
-	}
-
-	@Override
-	public boolean choiceExistsAsDirectChild(String choiceNameToFind) {
-
-		return fChoicesListHolder.choiceExists(choiceNameToFind);
-	}
-
-	@Override
-	public List<ChoiceNode> getLeafChoices() {
-
-		return ChoiceNodeHelper.getLeafChoices(getChoices());
-	}	
-
-	@Override
-	public List<ChoiceNode> getLeafChoicesWithCopies() { // TODO MO-RE do we need this ? rename ?
-
-		return ChoiceNodeHelper.getLeafChoices(getChoicesWithCopies());
-	}	
-
-	@Override
-	public Set<String> getAllChoiceNames() {
-
-		return ChoiceNodeHelper.getChoiceNames(getAllChoices());
-	}
-
-	@Override
-	public Set<String> getAllLabels() {
-
-		return ChoiceNodeHelper.getAllLabels(getAllChoices());
-	}
-
-	@Override
-	public Set<String> getLeafChoiceNames() {
-
-		return ChoiceNodeHelper.getChoiceNames(getLeafChoices());
-	}
-
-	@Override
-	public Set<ChoiceNode> getAllChoices() {
-
-		return ChoiceNodeHelper.getAllChoices(getChoices());
-	}
-
-	@Override
-	public Set<String> getChoiceNames() {
-
-		return ChoiceNodeHelper.getChoiceNames(getChoices());
-	}
-
-	@Override
-	public Set<ChoiceNode> getLabeledChoices(String label) {
-
-		return ChoiceNodeHelper.getLabeledChoices(label, getChoices());
-	}
-
-	@Override
-	public Set<String> getLeafLabels() { 
-
-		return ChoiceNodeHelper.getLeafLabels(getLeafChoices());
-	}
-
-	@Override
-	public Set<String> getLeafChoiceValues() {
-
-		return ChoiceNodeHelper.getLeafChoiceValues(getLeafChoices());
-	}
-
-	@Override
-	public boolean removeChoice(ChoiceNode choice) {
-
-		boolean result = fChoicesListHolder.removeChoice(choice);
-		registerChange();
-		return result;
-	}
-
-	@Override
-	public void replaceChoices(List<ChoiceNode> newChoices) {
-		
-		fChoicesListHolder.replaceChoices(newChoices, this);
-		registerChange();
-	}
-
-	@Override
-	public void clearChoices() {
-
-		fChoicesListHolder.clearChoices();
-	}
-
-}
+///*******************************************************************************
+// *
+// * Copyright (c) 2016 ecFeed AS.                                                
+// * All rights reserved. This program and the accompanying materials              
+// * are made available under the terms of the Eclipse Public License v1.0         
+// * which accompanies this distribution, and is available at                      
+// * http://www.eclipse.org/legal/epl-v10.html 
+// *  
+// *******************************************************************************/
+//
+//package com.ecfeed.core.model;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Set;
+//
+//public abstract class ChoicesParentNode extends AbstractNode implements IChoicesParentNode { // TODO MO-RE remove class
+//
+//	ChoicesListHolder fChoicesListHolder;
+//	
+//	public ChoicesParentNode(String name, IModelChangeRegistrator modelChangeRegistrator) {
+//		super(name, modelChangeRegistrator);
+//
+//		fChoicesListHolder = new ChoicesListHolder(modelChangeRegistrator);
+//	}
+//
+//	@Override
+//	public List<IAbstractNode> getChildren() {
+//
+//		return new ArrayList<IAbstractNode>(getChoices());
+//	}
+//
+//	@Override
+//	public int getChildrenCount() {
+//
+//		return getChoiceCount();
+//	}
+//
+//	@Override
+//	public boolean isMatch(IAbstractNode other) {
+//
+//		if (other instanceof ChoicesParentNode == false) {
+//			return false;
+//		}
+//
+//		ChoicesParentNode otherChoicesParentNode = (ChoicesParentNode)other;
+//
+//		if (!fChoicesListHolder.isMatch(otherChoicesParentNode.fChoicesListHolder)) {
+//			return false;
+//		}
+//
+//		boolean isMatch = super.isMatch(other);
+//
+//		if (!isMatch) {
+//			return false;
+//		}
+//		return true;
+//	}
+//
+//	@Override
+//	public void addChoice(ChoiceNode choiceToAdd) {
+//
+//		fChoicesListHolder.addChoice(choiceToAdd, this);
+//	}
+//
+//	@Override
+//	public void addChoice(ChoiceNode choiceToAdd, int index) {
+//
+//		fChoicesListHolder.addChoice(choiceToAdd, index, this);
+//		registerChange();
+//	}
+//
+//	@Override
+//	public void addChoices(List<ChoiceNode> choicesToAdd) {
+//
+//		fChoicesListHolder.addChoices(choicesToAdd, this);
+//		registerChange();
+//	}
+//
+//	@Override
+//	public List<ChoiceNode> getChoices() {
+//
+//		return fChoicesListHolder.getChoices();
+//	}
+//
+//	@Override
+//	public int getChoiceCount() {
+//
+//		return getChoices().size();
+//	}
+//
+//	public List<ChoiceNode> getChoicesWithCopies() { // TODO MO-RE do we need this ?
+//
+//		return getChoices();
+//	}
+//
+//	@Override
+//	public ChoiceNode getChoice(String qualifiedName) {
+//
+//		return (ChoiceNode)getChild(qualifiedName);
+//	}
+//
+//	@Override
+//	public int getChoiceIndex(String choiceNameToFind) {
+//
+//		return fChoicesListHolder.getChoiceIndex(choiceNameToFind);
+//	}
+//
+//	@Override
+//	public boolean choiceExistsAsDirectChild(String choiceNameToFind) {
+//
+//		return fChoicesListHolder.choiceExists(choiceNameToFind);
+//	}
+//
+//	@Override
+//	public List<ChoiceNode> getLeafChoices() {
+//
+//		return ChoiceNodeHelper.getLeafChoices(getChoices());
+//	}	
+//
+//	@Override
+//	public List<ChoiceNode> getLeafChoicesWithCopies() { // TODO MO-RE do we need this ? rename ?
+//
+//		return ChoiceNodeHelper.getLeafChoices(getChoicesWithCopies());
+//	}	
+//
+//	@Override
+//	public Set<String> getAllChoiceNames() {
+//
+//		return ChoiceNodeHelper.getChoiceNames(getAllChoices());
+//	}
+//
+//	@Override
+//	public Set<String> getAllLabels() {
+//
+//		return ChoiceNodeHelper.getAllLabels(getAllChoices());
+//	}
+//
+//	@Override
+//	public Set<String> getLeafChoiceNames() {
+//
+//		return ChoiceNodeHelper.getChoiceNames(getLeafChoices());
+//	}
+//
+//	@Override
+//	public Set<ChoiceNode> getAllChoices() {
+//
+//		return ChoiceNodeHelper.getAllChoices(getChoices());
+//	}
+//
+//	@Override
+//	public Set<String> getChoiceNames() {
+//
+//		return ChoiceNodeHelper.getChoiceNames(getChoices());
+//	}
+//
+//	@Override
+//	public Set<ChoiceNode> getLabeledChoices(String label) {
+//
+//		return ChoiceNodeHelper.getLabeledChoices(label, getChoices());
+//	}
+//
+//	@Override
+//	public Set<String> getLeafLabels() { 
+//
+//		return ChoiceNodeHelper.getLeafLabels(getLeafChoices());
+//	}
+//
+//	@Override
+//	public Set<String> getLeafChoiceValues() {
+//
+//		return ChoiceNodeHelper.getLeafChoiceValues(getLeafChoices());
+//	}
+//
+//	@Override
+//	public boolean removeChoice(ChoiceNode choice) {
+//
+//		boolean result = fChoicesListHolder.removeChoice(choice);
+//		registerChange();
+//		return result;
+//	}
+//
+//	@Override
+//	public void replaceChoices(List<ChoiceNode> newChoices) {
+//		
+//		fChoicesListHolder.replaceChoices(newChoices, this);
+//		registerChange();
+//	}
+//
+//	@Override
+//	public void clearChoices() {
+//
+//		fChoicesListHolder.clearChoices();
+//	}
+//
+//}
