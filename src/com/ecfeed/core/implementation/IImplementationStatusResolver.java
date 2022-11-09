993fa28aa2d5e7766b0618e145d61ecc02c10f3b
Marek Kuczabinski <m.kuczabinski@testify.no>
2022-11-09 08:58:48 +0100

    Code implementers commented-out.

--- src/com/ecfeed/core/implementation/IImplementationStatusResolver.java
+++ src/com/ecfeed/core/implementation/IImplementationStatusResolver.java
@@ -1,17 +1,17 @@
-/*******************************************************************************
- *
- * Copyright (c) 2016 ecFeed AS.                                                
- * All rights reserved. This program and the accompanying materials              
- * are made available under the terms of the Eclipse Public License v1.0         
- * which accompanies this distribution, and is available at                      
- * http://www.eclipse.org/legal/epl-v10.html 
- *  
- *******************************************************************************/
-
-package com.ecfeed.core.implementation;
-
-import com.ecfeed.core.model.IAbstractNode;
-
-public interface IImplementationStatusResolver {
-	public EImplementationStatus getImplementationStatus(IAbstractNode node);
-}
+///*******************************************************************************
+// *
+// * Copyright (c) 2016 ecFeed AS.                                                
+// * All rights reserved. This program and the accompanying materials              
+// * are made available under the terms of the Eclipse Public License v1.0         
+// * which accompanies this distribution, and is available at                      
+// * http://www.eclipse.org/legal/epl-v10.html 
+// *  
+// *******************************************************************************/
+//
+//package com.ecfeed.core.implementation;
+//
+//import com.ecfeed.core.model.IAbstractNode;
+//
+//public interface IImplementationStatusResolver {
+//	public EImplementationStatus getImplementationStatus(IAbstractNode node);
+//}
--- src/com/ecfeed/core/implementation/IModelImplementer.java
+++ src/com/ecfeed/core/implementation/IModelImplementer.java
@@ -1,20 +1,20 @@
-/*******************************************************************************
- *
- * Copyright (c) 2016 ecFeed AS.                                                
- * All rights reserved. This program and the accompanying materials              
- * are made available under the terms of the Eclipse Public License v1.0         
- * which accompanies this distribution, and is available at                      
- * http://www.eclipse.org/legal/epl-v10.html 
- *  
- *******************************************************************************/
-
-package com.ecfeed.core.implementation;
-
-import com.ecfeed.core.model.IAbstractNode;
-
-public interface IModelImplementer {
-	public boolean isImplementable(Class<? extends IAbstractNode> type);
-	public boolean isImplementable(IAbstractNode node);
-	public boolean implement(IAbstractNode node) throws Exception;
-	public EImplementationStatus getImplementationStatus(IAbstractNode node);
-}
+///*******************************************************************************
+// *
+// * Copyright (c) 2016 ecFeed AS.                                                
+// * All rights reserved. This program and the accompanying materials              
+// * are made available under the terms of the Eclipse Public License v1.0         
+// * which accompanies this distribution, and is available at                      
+// * http://www.eclipse.org/legal/epl-v10.html 
+// *  
+// *******************************************************************************/
+//
+//package com.ecfeed.core.implementation;
+//
+//import com.ecfeed.core.model.IAbstractNode;
+//
+//public interface IModelImplementer {
+//	public boolean isImplementable(Class<? extends IAbstractNode> type);
+//	public boolean isImplementable(IAbstractNode node);
+//	public boolean implement(IAbstractNode node) throws Exception;
+//	public EImplementationStatus getImplementationStatus(IAbstractNode node);
+//}
