af2727540e8671310ac497128460825ae967518d
Krzysztof <k.skorupski@testify.no>
2022-12-09 11:17:35 +0100

    package update

--- src/com/ecfeed/core/model/MethodDeployer.java
+++ src/com/ecfeed/core/model/MethodDeployer.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 import com.ecfeed.core.utils.ExceptionHelper;
 import com.ecfeed.core.utils.SignatureHelper;
-import sun.security.util.ConstraintsParameters;
 
 public abstract class MethodDeployer {
 
