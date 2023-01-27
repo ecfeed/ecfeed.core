ec6a65f376e51bb9b8b012d08d608466211c9adb
Marek Kuczabinski <m.kuczabinski@testify.no>
2023-01-27 12:16:03 +0100

    Android removed.

--- src/com/ecfeed/core/model/serialization/ModelParserForClass.java
+++ src/com/ecfeed/core/model/serialization/ModelParserForClass.java
@@ -15,11 +15,14 @@ import static com.ecfeed.core.model.serialization.SerializationConstants.CLASS_N
 import java.util.List;
 import java.util.Optional;
 
-import com.ecfeed.core.model.*;
-import com.ecfeed.core.utils.BooleanHolder;
+import com.ecfeed.core.model.BasicParameterNode;
+import com.ecfeed.core.model.ClassNode;
+import com.ecfeed.core.model.ClassNodeHelper;
+import com.ecfeed.core.model.CompositeParameterNode;
+import com.ecfeed.core.model.MethodNode;
+import com.ecfeed.core.model.RootNode;
 import com.ecfeed.core.utils.ExtLanguageManagerForJava;
 import com.ecfeed.core.utils.ListOfStrings;
-import com.ecfeed.core.utils.StringHolder;
 
 import nu.xom.Element;
 
@@ -33,17 +36,17 @@ public class ModelParserForClass implements IModelParserForClass {
 			IModelParserForGlobalParameter modelParserForGlobalParameter,
 			IModelParserForGlobalCompositeParameter modelParserForGlobalCompositeParameter,
 			IModelParserForMethod modelParserForMethod) {
-		
+
 		fModelParserForGlobalParameter = modelParserForGlobalParameter;
 		fModelParserForGlobalCompositeParameter = modelParserForGlobalCompositeParameter;
 		fModelParserForMethod = modelParserForMethod;
 	}
-	
+
 	public Optional<ClassNode> parseClass(
 			Element classElement, RootNode parent, ListOfStrings errorList) throws ParserException {
 
 		String name;
-		
+
 		try {
 			ModelParserHelper.assertNameEqualsExpectedName(classElement.getQualifiedName(), CLASS_NODE_NAME, errorList);
 			name = ModelParserHelper.getElementName(classElement, errorList);
@@ -51,15 +54,7 @@ public class ModelParserForClass implements IModelParserForClass {
 			return Optional.empty();
 		}
 
-		BooleanHolder runOnAndroidHolder = new BooleanHolder(false);
-		StringHolder androidBaseRunnerHolder = new StringHolder(); 
-
-		ClassNode targetClassNode = 
-				new ClassNode(
-						name, 
-						parent.getModelChangeRegistrator(), 
-						runOnAndroidHolder.get(), 
-						androidBaseRunnerHolder.get());
+		ClassNode targetClassNode = new ClassNode(name, parent.getModelChangeRegistrator());
 
 		targetClassNode.setDescription(ModelParserHelper.parseComments(classElement));
 		//we need to do it here, so the backward search for global parameters will work
@@ -73,81 +68,81 @@ public class ModelParserForClass implements IModelParserForClass {
 	}
 
 	private void parseClassParameters(Element classElement, ClassNode targetClassNode, ListOfStrings errorList) {
-		
+
 		List<Element> iterableChildren = 
 				ModelParserHelper.getIterableChildren(
 						classElement, SerializationHelperVersion1.getParameterNodeNames());
-		
+
 		for (Element child : iterableChildren) {
-			
+
 			parseClassParameter(targetClassNode, errorList, child);
 		}
 	}
 
 	private void parseClassParameter(ClassNode targetClassNode, ListOfStrings errorList, Element child) {
-		
+
 		boolean isBasicParameter = 
 				ModelParserHelper.verifyElementName(
 						child, SerializationHelperVersion1.getBasicParameterNodeName());
-		
+
 		if (isBasicParameter) {
-			
+
 			Optional<BasicParameterNode> globalBasicParameter = 
 					fModelParserForGlobalParameter.parseGlobalBasicParameter(
 							child, targetClassNode.getModelChangeRegistrator(), errorList);
-			
+
 			globalBasicParameter.ifPresent(targetClassNode::addParameter);
 			return;
 		} 
-		
+
 		boolean isCompositeParameter = 
 				ModelParserHelper.verifyElementName(
 						child, SerializationHelperVersion1.getCompositeParameterNodeName());
-		
+
 		if (isCompositeParameter) {
-			
+
 			Optional<CompositeParameterNode> globalCompositeParameter = 
 					fModelParserForGlobalCompositeParameter.parseGlobalCompositeParameter(
 							child, targetClassNode.getModelChangeRegistrator(), errorList);
-			
+
 			globalCompositeParameter.ifPresent(targetClassNode::addParameter);
 		}
 	}
 
 	private void parseMethods(
 			Element classElement, ClassNode targetClassNode, ListOfStrings errorList)
-			throws ParserException {
-		
+					throws ParserException {
+
 		List<Element> childrenMethodElements = 
 				ModelParserHelper.getIterableChildren(classElement, SerializationConstants.METHOD_NODE_NAME);
-		
+
 		for (Element methodElement : childrenMethodElements) {
-			
+
 			Optional<MethodNode> node = fModelParserForMethod.parseMethod(methodElement, targetClassNode, errorList);
-			
+
 			if (!node.isPresent()) {
 				continue;
 			}
-				
+
 			MethodNode methodNode = node.get();
-			
+
 			addMethodWithUniqueName(targetClassNode, methodNode);
 		}
 	}
 
 	private void addMethodWithUniqueName(ClassNode targetClassNode, MethodNode methodNode) {
-		
+
 		MethodNode existingMethodNode = targetClassNode.findMethodWithTheSameName(methodNode.getName());
-		
+
 		if (existingMethodNode != null) {
-			
+
 			String newMethodName = 
 					ClassNodeHelper.generateNewMethodName(
 							targetClassNode, existingMethodNode.getName(), new ExtLanguageManagerForJava());
-			
+
 			methodNode.setName(newMethodName);
 		}
-			
+
 		targetClassNode.addMethod(methodNode);
 	}
 
