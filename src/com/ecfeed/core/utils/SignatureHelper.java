ae9c0fb4ee5f127fdd8ceedcdb3d6050efe84894
Marek Kuczabinski <m.kuczabinski@testify.no>
2023-03-29 10:59:43 +0200

    Signature root marker added.

--- src/com/ecfeed/core/utils/SignatureHelper.java
+++ src/com/ecfeed/core/utils/SignatureHelper.java
@@ -20,5 +20,6 @@ public class SignatureHelper {
 	public static final String SIGNATURE_NAME_SEPARATOR = ":";  // separates sections of composite 
 	public static final String SIGNATURE_TYPE_SEPARATOR = " : ";  // name from type
 	public static final String SIGNATURE_CONTENT_SEPARATOR = " : "; // name from content
+	public static final String SIGNATURE_ROOT_MARKER = "@";  // informs that the first item is root node
 
 }
