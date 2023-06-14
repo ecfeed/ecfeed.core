b30789571f6ebcdb2b1aa43b3603c32ad135c254
Marek Kuczabinski <m.kuczabinski@testify.no>
2023-06-14 08:39:07 +0200

    Code cleanup.

--- test/com/ecfeed/core/serialization/ModelSerializerWithConstraintsTest.java
+++ test/com/ecfeed/core/serialization/ModelSerializerWithConstraintsTest.java
@@ -13,7 +13,7 @@ package com.ecfeed.core.serialization;
 //
 //import static com.ecfeed.core.testutils.ModelTestUtils.assertElementsEqual;
 
-public class ModelSerializerWithConstraintsTest { // XYX
+public class ModelSerializerWithConstraintsTest {
 
 	// TODO MO-RE remove? test with constraints are ModelSerializerAndParserTest.java
 	// create tests for individual types of constraints / statements / conditions in ModelSerializerAndParserTest.java 
