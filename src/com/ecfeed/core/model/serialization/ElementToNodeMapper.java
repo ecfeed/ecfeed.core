8723320892691d5d2ca22712995a511d82e67a40
Marek Kuczabinski <m.kuczabinski@testify.no>
2023-07-25 12:26:06 +0200

    Mapper - toString method added.

--- src/com/ecfeed/core/model/serialization/ElementToNodeMapper.java
+++ src/com/ecfeed/core/model/serialization/ElementToNodeMapper.java
@@ -10,22 +10,42 @@
 
 package com.ecfeed.core.model.serialization;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.ecfeed.core.model.AbstractNode;
 
 public class ElementToNodeMapper {
-	
+
 	private final Map<nu.xom.Element, AbstractNode> fElementsToNodes = new HashMap<>();
-	
+
+	@Override
+	public String toString() {
+
+		return "(" + fElementsToNodes.size() + ")  " + getSignatureOfElements(); 
+	}
+
+	private String getSignatureOfElements() {
+
+		List<String> signatures = new ArrayList<>();
+
+		for (nu.xom.Element key : fElementsToNodes.keySet()) {
+			String signatureElement = getNode(key).getName();
+			signatures.add(signatureElement);
+		}
+
+		return signatures.toString();
+	}
+
 	public void addMappings(nu.xom.Element xomElement, AbstractNode abstractNode) {
-		
+
 		fElementsToNodes.put(xomElement, abstractNode);
 	}
-	
+
 	public AbstractNode getNode(nu.xom.Element xomElement) {
-		
+
 		return fElementsToNodes.get(xomElement);
 	}
 
