d01c3d1a20eb9201eeb0191fc4d7d23ea6873642
Marek Kuczabinski <m.kuczabinski@testify.no>
2023-07-27 12:14:51 +0200

    Property method runner removed.

--- src/com/ecfeed/core/model/serialization/XomBuilder.java
+++ src/com/ecfeed/core/model/serialization/XomBuilder.java
@@ -666,7 +666,7 @@ public abstract class XomBuilder implements IModelVisitor {
 
 	private void addMethodProperties(MethodNode methodNode, Element targetElement) {
 
-		addMethodProperty(NodePropertyDefs.PropertyId.PROPERTY_METHOD_RUNNER, methodNode, targetElement);
+		//addMethodProperty(NodePropertyDefs.PropertyId.PROPERTY_METHOD_RUNNER, methodNode, targetElement);
 		addMethodProperty(NodePropertyDefs.PropertyId.PROPERTY_MAP_BROWSER_TO_PARAM,  methodNode, targetElement);
 		addMethodProperty(NodePropertyDefs.PropertyId.PROPERTY_WEB_BROWSER, methodNode, targetElement);
 		addMethodProperty(NodePropertyDefs.PropertyId.PROPERTY_BROWSER_DRIVER_PATH, methodNode, targetElement);
