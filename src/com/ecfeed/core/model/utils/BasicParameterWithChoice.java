507186328ceb17309fb2460258dc4657f10605c1
Marek Kuczabinski <m.kuczabinski@testify.no>
2023-08-30 11:26:30 +0200

    Basic parameter with choice added.

---
+++ src/com/ecfeed/core/model/utils/BasicParameterWithChoice.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2016 ecFeed AS.                                                
+ * All rights reserved. This program and the accompanying materials              
+ * are made available under the terms of the Eclipse Public License v1.0         
+ * which accompanies this distribution, and is available at                      
+ * http://www.eclipse.org/legal/epl-v10.html 
+ *  
+ *******************************************************************************/
+
+package com.ecfeed.core.model.utils;
+
+import com.ecfeed.core.model.BasicParameterNode;
+import com.ecfeed.core.model.ChoiceNode;
+
+public class BasicParameterWithChoice {
+
+	private BasicParameterNode fBasicParameterNode;
+	private ChoiceNode fChoiceNode;
+
+	public BasicParameterWithChoice(BasicParameterNode basicParameterNode, ChoiceNode choiceNode) {
+
+		fBasicParameterNode = basicParameterNode;
+		fChoiceNode = choiceNode;
+	}
+
+	public BasicParameterNode getBasicParameterNode() {
+		return fBasicParameterNode;
+	}
+
+	public ChoiceNode getChoiceNode() {
+		return fChoiceNode;
+	}
+
+}
