57b787a8f3000372db8563cea9ddb7885fe0cdd3
Krzysztof Skorupski <k.skorupski@testify.no>
2023-09-07 14:44:14 +0200

    file preview

--- src/com/ecfeed/core/parser/model/export/ModelDataExport.java
+++ src/com/ecfeed/core/parser/model/export/ModelDataExport.java
@@ -4,16 +4,31 @@ import com.ecfeed.core.model.MethodNode;
 import com.ecfeed.core.model.TestCaseNode;
 import com.ecfeed.core.model.TestSuiteNode;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public interface ModelDataExport {
 
-    String getFilePreview(List<TestCaseNode> suite);
     String getFile(List<TestCaseNode> suite);
     String getFile(TestSuiteNode suite);
     Optional<String> getHeader(MethodNode method);
     Optional<String> getFooter(MethodNode method);
     String getTest(TestCaseNode test);
     String getTest(TestCaseNode test, int index);
+
+    default String getFilePreview(List<TestCaseNode> suite) {
+
+        if (suite.size() == 0) {
+            throw new RuntimeException("The test suite should consist of at least one test case!");
+        }
+
+        List<TestCaseNode> suiteUpdated = new ArrayList<>();
+
+        for (int i = 0 ; i < suite.size() && i < 2 ; i++) {
+            suiteUpdated.add(suite.get(0));
+        }
+
+        return getFile(suiteUpdated);
+    }
 }
